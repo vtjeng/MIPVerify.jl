12204e6ce248ac6f39353b7f697f65ac9a10de42
Vincent Tjeng <vtjeng@mit.edu>
2017-12-15 16:33:16 -0500

    Run subset of tests if Gurobi is not available.
      - Full suite of tests with Cbc takes ~5 hours (including model build time).

--- test/integration.jl
+++ test/integration.jl
@@ -2,5 +2,9 @@ using Base.Test
 
 @testset "integration tests" begin
     include("integration/generated_weights.jl")
-    include("integration/trained_weights.jl")
+    if Pkg.installed("Gurobi") != nothing
+        # Skip these tests if Gurobi is not installed.
+        # The corresponding networks are too large for CBC to deal with.
+        include("integration/trained_weights.jl")
+    end
 end
