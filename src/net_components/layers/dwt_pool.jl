5def6a0f8499e6ddc0ec190deab08290b5b33efb
wzh <wangze@mail.ustc.edu.cn>
2019-07-20 23:09:25 +0800

    dwtpooling

--- src/net_components/layers/dwt_pool.jl
+++ src/net_components/layers/dwt_pool.jl
@@ -15,12 +15,12 @@ end
 
 function apply(x::Array{<:JuMPReal})
     println("apply DWT_Pooling to shape", size(x))
-    out = zeros((Int(size(x)[1]/2), (Int(size(x)[1]/2), 3)))
+    out = zeros((1, Int(size(x)[1]/2), (Int(size(x)[1]/2), 3)))
     for i=1:size(x)[3]
         wt = wavelet(WT.haar)
         wt = WT.scale(wt, 1/sqrt(2))
-        x1 = dwt(x[:,:,i], wt, 1)
-        out[:,:,i] = x1[1:Int(size(x1)[1]/2), 1:Int(size(x1)[2]/2)]
+        x1 = dwt(x[1,:,:,i], wt, 1)
+        out[1,:,:,i] = x1[1:Int(size(x1)[1]/2), 1:Int(size(x1)[2]/2)]
     end
     return out
 end
