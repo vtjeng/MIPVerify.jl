3e82c06d4b82b38331b0649643e730f04baf5332
Vincent Tjeng <vincent.tjeng@gmail.com>
2019-08-17 14:30:03 -0700

    Refine and streamline integration tests.

--- test/integration/sequential/generated_weights.jl
+++ test/integration/sequential/generated_weights.jl
@@ -1,12 +1,6 @@
 using Test
 
 @testset "generated_weights" begin
-    include("generated_weights/fc+softmax.jl")
     include("generated_weights/mfc+mfc+softmax.jl")
     include("generated_weights/conv+fc+softmax.jl")
-
-    if Base.find_package("Gurobi") != nothing
-        # Skip these tests if Gurobi is not installed.
-        include("generated_weights/conv+conv+fc+softmax.jl")
-    end
 end
--- test/integration/sequential/trained_weights/MNIST.n1.jl
+++ test/integration/sequential/trained_weights/MNIST.n1.jl
@@ -1,6 +1,6 @@
 using Test
 using MIPVerify
-using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily
+using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LInfNormBoundedPerturbationFamily
 using MIPVerify: get_example_network_params, read_datasets, get_image
 @isdefined(TestHelpers) || include("../../../TestHelpers.jl")
 
@@ -12,39 +12,16 @@ using MIPVerify: get_example_network_params, read_datasets, get_image
     pp_unrestricted = UnrestrictedPerturbationFamily()
 
     sample_index = 1
-    @testset "sample index = $sample_index" begin
-        x0 = get_image(mnist.test.images, sample_index)
+    x0 = get_image(mnist.test.images, sample_index)
 
-        expected_objective_values = Dict(
-            (1, pp_unrestricted, 1, 0) => 13.8219,
-            (1, pp_unrestricted, Inf, 0) => 0.0964639,
-            (3, pp_blur, 1, 0) => 88.0038,
-            (3, pp_blur, Inf, 0) => 0.973011,
-            (10, pp_unrestricted, 1, 0) => 4.64186,
-            (10, pp_unrestricted, Inf, 0) => 0.0460847,
-            (2, pp_blur, 1, 0) => NaN,
-            (2, pp_blur, Inf, 0) => NaN,
-            (5, pp_blur, 1, 0) => NaN,
-            (5, pp_blur, Inf, 0) => NaN,
-            (6, pp_blur, 1, 0) => NaN,
-            (6, pp_blur, Inf, 0) => NaN
-        )
-    
-        TestHelpers.batch_test_adversarial_example(nn, x0, expected_objective_values)
-    end
+    @testset "Basic integration test demonstrating success on trained (non-robust) network." begin
+        test_cases = [
+            ((10, pp_unrestricted, Inf, 0), 0.0460847),
+            ((10, LInfNormBoundedPerturbationFamily(0.04), Inf, 0), NaN),
+            ((10, LInfNormBoundedPerturbationFamily(0.05), Inf, 0), 0.0460847),
+        ]
 
-    sample_index = 3
-    @testset "sample index = $sample_index" begin
-        x0 = get_image(mnist.test.images, sample_index)
-        
-        expected_objective_values = Dict(
-            (8, pp_unrestricted, 1, 0) => 0.582587,
-            (8, pp_unrestricted, Inf, 0) => 0.00363594,
-            (8, pp_blur, 1, 0) => 9.77784,
-            (8, pp_blur, Inf, 0) => 0.277525
-        )
-        
-        TestHelpers.batch_test_adversarial_example(nn, x0, expected_objective_values)
+        TestHelpers.batch_test_adversarial_example(nn, x0, test_cases)
     end
     
 end
