e4c891307a98b33519eab1955da4c6cbb6284e5c
Vincent Tjeng <vincent.tjeng@gmail.com>
2019-08-17 10:59:21 -0700

    Remove random behavior from test/../sequential.jl

--- test/net_components/nets/sequential.jl
+++ test/net_components/nets/sequential.jl
@@ -1,18 +1,16 @@
 using Test
-using JuMP
 using MIPVerify
-using Random
 
 @testset "sequential.jl" begin
 
     @testset "Sequential" begin
         nnparams = Sequential([
-            Conv2d(rand(Float64, 1, 4, 4, 16), rand(Float64, 16), 2),
+            Conv2d(ones(1, 4, 4, 16), ones(16), 2),
             ReLU(),
             Flatten([3, 4, 1, 2]),
-            Linear(rand(Float64, 1000, 50), rand(Float64, 50)),
-            MaskedReLU(rand(Random.MersenneTwister(0), [-1, 0, 1], 50)),
-            Linear(rand(Float64, 50, 10), rand(Float64, 10))
+            Linear(ones(1000, 20), ones(20)),
+            MaskedReLU([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0]),
+            Linear(ones(20, 10), ones(10))
         ], "testnet")
         io = IOBuffer()
         Base.show(io, nnparams)
@@ -21,9 +19,9 @@ using Random
           (1) Conv2d(4, 16, kernel_size=(1, 4), stride=(2, 2), padding=same)
           (2) ReLU()
           (3) Flatten(): flattens 4 dimensional input, with dimensions permuted according to the order [3, 4, 1, 2]
-          (4) Linear(1000 -> 50)
-          (5) MaskedReLU with expected input size (50,). (16 zeroed, 17 as-is, 17 rectified).
-          (6) Linear(50 -> 10)
+          (4) Linear(1000 -> 20)
+          (5) MaskedReLU with expected input size (20,). (10 zeroed, 7 as-is, 3 rectified).
+          (6) Linear(20 -> 10)
         """
     end
 
