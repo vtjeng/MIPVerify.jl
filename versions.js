05f5b9cfd5bfd179925c6b7640db8185c146910b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-18 06:34:05 +0000

    build based on b8489c8

--- versions.js
+++ versions.js
@@ -1,12 +1,5 @@
 var DOC_VERSIONS = [
   "stable",
-  "latest",
-  "release-0.1",
-  "release-0.0",
-  "v0.1.1",
-  "v0.1.0",
-  "v0.0.8",
-  "v0.0.7",
-  "v0.0.6",
-  "v0.0.5",
+  "v0.1",
+  "dev",
 ];
