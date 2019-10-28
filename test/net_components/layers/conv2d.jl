de937d06dc8bcc9316452e729e4fc939242c30fa
David Shriver <dls2fc@virginia.edu>
2019-10-28 13:16:18 -0400

    fix same padding computation

--- test/net_components/layers/conv2d.jl
+++ test/net_components/layers/conv2d.jl
@@ -115,4 +115,92 @@ using MIPVerify: check_size, increment!
             @test solve_output≈true_output
         end
     end
+
+    @testset "conv2d with non-unit stride" begin
+        input_size = (1, 6, 6, 2)
+        input = reshape(collect(1:prod(input_size)), input_size) .- 36
+        filter_size = (3, 3, 2, 1)
+        filter = reshape(collect(1:prod(filter_size)), filter_size) .- 9
+        bias_size = (1, )
+        bias = [1]
+        stride = 2
+        true_output_raw = [
+            1597  1615  1120;
+            1705  1723  1120;
+            903   879   513 ;
+        ]
+        true_output = reshape(transpose(true_output_raw), (1, 3, 3, 1))
+        p = Conv2d(filter, bias, stride)
+        @testset "Numerical Input, Numerical Layer Parameters" begin
+            evaluated_output = MIPVerify.conv2d(input, p)
+            @test evaluated_output == true_output
+        end
+        @testset "Numerical Input, Variable Layer Parameters" begin
+            m = TestHelpers.get_new_model()
+            filter_v = map(_ -> @variable(m), CartesianIndices(filter_size))
+            bias_v = map(_ -> @variable(m), CartesianIndices(bias_size))
+            p_v = Conv2d(filter_v, bias_v, stride)
+            output_v = MIPVerify.conv2d(input, p_v)
+            @constraint(m, output_v .== true_output)
+            solve(m)
+
+            p_solve = MIPVerify.Conv2d(getvalue(filter_v), getvalue(bias_v), stride)
+            solve_output = MIPVerify.conv2d(input, p_solve)
+            @test solve_output≈true_output
+        end
+        @testset "Variable Input, Numerical Layer Parameters" begin
+            m = TestHelpers.get_new_model()
+            input_v = map(_ -> @variable(m), CartesianIndices(input_size))
+            output_v = MIPVerify.conv2d(input_v, p)
+            @constraint(m, output_v .== true_output)
+            solve(m)
+
+            solve_output = MIPVerify.conv2d(getvalue(input_v), p)
+            @test solve_output≈true_output
+        end
+    end
+
+    @testset "conv2d with stride 2, odd input shape with even filter shape" begin
+        input_size = (1, 5, 5, 2)
+        input = reshape(collect(1:prod(input_size)), input_size) .- 25
+        filter_size = (4, 4, 2, 1)
+        filter = reshape(collect(1:prod(filter_size)), filter_size) .- 16
+        bias_size = (1, )
+        bias = [1]
+        stride = 2
+        true_output_raw = [
+            1756  2511  1310;
+            3065  4097  1969;
+            1017  1225  501 ;
+        ]
+        true_output = reshape(transpose(true_output_raw), (1, 3, 3, 1))
+        p = Conv2d(filter, bias, stride)
+        @testset "Numerical Input, Numerical Layer Parameters" begin
+            evaluated_output = MIPVerify.conv2d(input, p)
+            @test evaluated_output == true_output
+        end
+        @testset "Numerical Input, Variable Layer Parameters" begin
+            m = TestHelpers.get_new_model()
+            filter_v = map(_ -> @variable(m), CartesianIndices(filter_size))
+            bias_v = map(_ -> @variable(m), CartesianIndices(bias_size))
+            p_v = Conv2d(filter_v, bias_v, stride)
+            output_v = MIPVerify.conv2d(input, p_v)
+            @constraint(m, output_v .== true_output)
+            solve(m)
+
+            p_solve = MIPVerify.Conv2d(getvalue(filter_v), getvalue(bias_v), stride)
+            solve_output = MIPVerify.conv2d(input, p_solve)
+            @test solve_output≈true_output
+        end
+        @testset "Variable Input, Numerical Layer Parameters" begin
+            m = TestHelpers.get_new_model()
+            input_v = map(_ -> @variable(m), CartesianIndices(input_size))
+            output_v = MIPVerify.conv2d(input_v, p)
+            @constraint(m, output_v .== true_output)
+            solve(m)
+
+            solve_output = MIPVerify.conv2d(getvalue(input_v), p)
+            @test solve_output≈true_output
+        end
+    end
 end
