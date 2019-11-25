68bc427aca33f11301c048749157060ad773f511
Vincent Tjeng <vincent.tjeng@gmail.com>
2019-11-24 23:14:30 -0800

    Simplify conv+fc+softmax.jl so they take about a minute to run.

--- test/integration/sequential/generated_weights/conv+fc+softmax.jl
+++ test/integration/sequential/generated_weights/conv+fc+softmax.jl
@@ -6,8 +6,8 @@ using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LIn
 @timed_testset "conv+fc+softmax.jl" begin
     ### Parameters for neural net
     batch = 1
-    c1_in_height = 8
-    c1_in_width = 8
+    c1_in_height = 6
+    c1_in_width = 6
     p1_stride_height = 2
     p1_stride_width = 2
     p1_strides = (1, p1_stride_height, p1_stride_width, 1)
@@ -18,7 +18,7 @@ using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LIn
     c1_filter_width = 2
     c1_out_channels = 2
 
-    l1_height = 5
+    l1_height = 4
     l1_width = p1_height*p1_width*c1_out_channels
 
     l2_height = 3
@@ -56,7 +56,7 @@ using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LIn
         pp_blur = BlurringPerturbationFamily((5, 5))
         # TODO (vtjeng): Add example where blurring perturbation generates non-NaN results
         test_cases = [
-            ((2, pp_blur, 1, 0), NaN),
+            ((2, pp_blur, 1, 0), 6.959316),
             ((3, pp_blur, 1, 0), NaN),
         ]
 
@@ -68,7 +68,7 @@ using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LIn
         @timed_testset "Minimizing l1 norm" begin
             test_cases = [
                 ((1, pp_unrestricted, 1, 0), 0),
-                ((2, pp_unrestricted, 1, 0), 9.2825418),
+                ((2, pp_unrestricted, 1, 0), 0.9187638),
             ]
 
             TestHelpers.batch_test_adversarial_example(nn, input, test_cases)
@@ -77,8 +77,8 @@ using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LIn
         @timed_testset "Minimizing lInf norm" begin
             test_cases = [
                 ((1, pp_unrestricted, Inf, 0), 0),
-                ((2, pp_unrestricted, Inf, 0), 0.4347241),
-                ((3, pp_unrestricted, Inf, 0), 0.5045819),
+                ((2, pp_unrestricted, Inf, 0), 0.06688736),
+                ((3, pp_unrestricted, Inf, 0), 0.4270584),
             ]
 
             TestHelpers.batch_test_adversarial_example(nn, input, test_cases)
@@ -86,7 +86,7 @@ using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LIn
 
         @timed_testset "Increasing margin increases required distance" begin
             test_cases = [
-                ((2, pp_unrestricted, 1, 0.1), 9.5642628),
+                ((2, pp_unrestricted, 1, 0.1), 0.98717927),
             ]
 
             TestHelpers.batch_test_adversarial_example(nn, input, test_cases)
@@ -94,7 +94,7 @@ using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LIn
 
         @timed_testset "With multiple target labels specified, minimum target label found" begin
             test_cases = [
-                (([2, 3], pp_unrestricted, Inf, 0), 0.4347241),
+                (([2, 3], pp_unrestricted, Inf, 0), 0.06688736),
             ]
 
             TestHelpers.batch_test_adversarial_example(nn, input, test_cases)
@@ -103,8 +103,8 @@ using MIPVerify: UnrestrictedPerturbationFamily, BlurringPerturbationFamily, LIn
 
     @timed_testset "LInfNormBoundedPerturbationFamily" begin
         test_cases = [
-            ((2, LInfNormBoundedPerturbationFamily(0.43), Inf, 0), NaN),  # restricting maximum perturbation to below minimum distance causes optimization problem to be infeasible
-            ((2, LInfNormBoundedPerturbationFamily(0.435), Inf, 0), 0.4347241),  # restricting maximum perturbation to above minimum distance does not affect optimal value of problem
+            ((2, LInfNormBoundedPerturbationFamily(0.06), Inf, 0), NaN),  # restricting maximum perturbation to below minimum distance causes optimization problem to be infeasible
+            ((2, LInfNormBoundedPerturbationFamily(0.07), Inf, 0), 0.06688736),  # restricting maximum perturbation to above minimum distance does not affect optimal value of problem
         ]
 
         TestHelpers.batch_test_adversarial_example(nn, input, test_cases)
