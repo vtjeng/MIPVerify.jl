191f4b21aa0c56e62d52d0c129dee7f165c7f927
Vincent Tjeng <vincent.tjeng@gmail.com>
2020-05-25 12:47:00 -0700

    Split command over multiple lines.

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,4 +3,13 @@
 # Determining root directory of the repo: https://stackoverflow.com/a/957978
 repo_root=$(git rev-parse --show-toplevel)
 
-(cd "${repo_root}" && julia -e 'using Pkg; Pkg.activate(tempname()); Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); using JuliaFormatter; format(".", verbose=true)')
+(
+  cd "${repo_root}" &&
+  julia -e '
+    using Pkg;
+    Pkg.activate(tempname());
+    Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); 
+    using JuliaFormatter;
+    format(".", verbose=true)
+  '
+)
