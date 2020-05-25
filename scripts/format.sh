bc8605694ba22ce89bb5a46823f35ce5526d35cf
Vincent Tjeng <vincent.tjeng@gmail.com>
2020-05-25 13:53:51 -0700

    [scripts] fix format checker (#52)
    + Make identification of repo root more robust.
    + Improve readability of multiline `julia` command.

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# per https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Determining root directory of the repo: https://stackoverflow.com/a/957978
+repo_root=$(git rev-parse --show-toplevel)
 
-(cd $DIR/.. && julia -e 'using Pkg; Pkg.activate(tempname()); Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); using JuliaFormatter; format(".", verbose=true)')
+(
+  cd "${repo_root}" &&
+  julia -e '
+    using Pkg;
+    Pkg.activate(tempname());
+    Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); 
+    using JuliaFormatter;
+    format(".", verbose=true)
+  '
+)
