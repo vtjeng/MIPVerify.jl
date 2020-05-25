f03257212517fe0d92debe93063ba1234a818194
Vincent Tjeng <vincent.tjeng@gmail.com>
2020-05-25 12:14:49 -0700

    Double quote variable to prevent globbing and word splitting.

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,4 +3,4 @@
 # Determining root directory of the repo: https://stackoverflow.com/a/957978
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-(cd $REPO_ROOT && julia -e 'using Pkg; Pkg.activate(tempname()); Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); using JuliaFormatter; format(".", verbose=true)')
+(cd "${REPO_ROOT}" && julia -e 'using Pkg; Pkg.activate(tempname()); Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); using JuliaFormatter; format(".", verbose=true)')
