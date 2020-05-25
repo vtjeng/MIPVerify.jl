637a24568b9480f3df23274bd3d519e8bf8f4a44
Vincent Tjeng <vincent.tjeng@gmail.com>
2020-05-25 12:18:50 -0700

    Lowercase since it is a local variable.

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Determining root directory of the repo: https://stackoverflow.com/a/957978
-REPO_ROOT=$(git rev-parse --show-toplevel)
+repo_root=$(git rev-parse --show-toplevel)
 
-(cd "${REPO_ROOT}" && julia -e 'using Pkg; Pkg.activate(tempname()); Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); using JuliaFormatter; format(".", verbose=true)')
+(cd "${repo_root}" && julia -e 'using Pkg; Pkg.activate(tempname()); Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); using JuliaFormatter; format(".", verbose=true)')
