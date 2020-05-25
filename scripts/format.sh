63f778c0ac720a3d4a234c630a28f1370659ddcc
Vincent Tjeng <vincent.tjeng@gmail.com>
2020-05-25 12:07:56 -0700

    Make navigation to repo root more reliable.

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# per https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Determining root directory of the repo: https://stackoverflow.com/a/957978
+REPO_ROOT=$(git rev-parse --show-toplevel)
 
-(cd $DIR/.. && julia -e 'using Pkg; Pkg.activate(tempname()); Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); using JuliaFormatter; format(".", verbose=true)')
+(cd $REPO_ROOT && julia -e 'using Pkg; Pkg.activate(tempname()); Pkg.add(PackageSpec(name="JuliaFormatter", version="0.5.4")); using JuliaFormatter; format(".", verbose=true)')
