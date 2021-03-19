377d3d0cc68f6bf97bbc49a478b19cfd3db1c33b
Vincent Tjeng <vincent.tjeng@gmail.com>
2021-03-18 18:26:12 -0700

    Fix Windows situation.

--- src/utils/prep_data_file.jl
+++ src/utils/prep_data_file.jl
@@ -10,12 +10,13 @@ function prep_data_file(relative_dir::String, filename::String)::String
     absolute_file_path = joinpath(dependencies_path, relative_file_path)
     if !isfile(absolute_file_path)
         if Sys.iswindows()
-            # on windows, the `joinpath` command uses `\` as a separator.
+            # On windows, the `joinpath` command uses `\` as a separator.
+            # TODO: This is a bit of a hack; we might prefer rethinking `relative_dir`
             relative_url_path = replace(relative_file_path, "\\" => "/")
         else
             relative_url_path = relative_file_path
         end
-        url = joinpath(data_repo_path, relative_url_path)
+        url = string(data_repo_path, "/", relative_url_path)
         download(url, absolute_file_path)
     end
 
