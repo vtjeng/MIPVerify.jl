e86b47b256b27af27bec5b72403f00bbe1835c46
anpere <andres15perez@gmail.com>
2024-01-02 06:12:37 +0000

    change test

--- test/net_components/layers/zero.jl
+++ test/net_components/layers/zero.jl
@@ -3,16 +3,16 @@ using Test
 @testset "zero.jl" begin
 
     @testset "Zero" begin
+    	 @testset "Flatten" begin
+	     p = Zero()
+             @test p() == 0
+	 end
         @testset "Base.show" begin
             p = Zero()
             io = IOBuffer()
             Base.show(io, p)
             @test String(take!(io)) == "Zero()"
         end
-	@testset "Initialize with tightening algorithm" begin
-	    p = Zero([1,2,3])
-	    @test p === 0
-	end
     end
 
 end
